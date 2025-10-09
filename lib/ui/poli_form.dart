62b14a88d7dfab10a61007ee4e34f024adfa252c
rhyru9 <reyhansyah4@gmail.com>
2025-10-09 15:16:29 +0700

    mobprooooo

--- lib/ui/poli_form.dart
+++ lib/ui/poli_form.dart
@@ -1,15 +1,18 @@
 // ui/poli_form.dart
 import 'package:flutter/material.dart';
+import '../model/poli.dart';
+import 'poli_detail.dart';
 
 class PoliForm extends StatefulWidget {
-  const PoliForm({super.key});
+  const PoliForm({Key? key}) : super(key: key);
 
   @override
-  State<PoliForm> createState() => _PoliFormState();
+  _PoliFormState createState() => _PoliFormState();
 }
 
 class _PoliFormState extends State<PoliForm> {
   final _formKey = GlobalKey<FormState>();
+  final _namaPoliCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +23,41 @@ class _PoliFormState extends State<PoliForm> {
           key: _formKey,
           child: Column(
             children: [
-              const TextField(
-                decoration: InputDecoration(labelText: "Nama Poli"),
-              ),
+              _fieldNamaPoli(),
               const SizedBox(height: 20),
-              ElevatedButton(onPressed: () {}, child: const Text("Simpan")),
+              _tombolSimpan(),
             ],
           ),
         ),
       ),
     );
+  }
+
+  // Fungsi untuk TextField Nama Poli
+  _fieldNamaPoli() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nama Poli"),
+      controller: _namaPoliCtrl,
+    );
+  }
+
+  // Fungsi untuk Tombol Simpan
+  _tombolSimpan() {
+    return ElevatedButton(
+      onPressed: () {
+        Poli poli = Poli(namaPoli: _namaPoliCtrl.text);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => PoliDetail(poli: poli)),
+        );
+      },
+      child: const Text("Simpan"),
+    );
+  }
+
+  @override
+  void dispose() {
+    _namaPoliCtrl.dispose();
+    super.dispose();
   }
 }
